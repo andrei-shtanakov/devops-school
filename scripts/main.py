7dac7db010fa52ae3a1bb3d6b9d2e67e546cfca0
Andrei Shtanakov <45091754+andrei-shtanakov@users.noreply.github.com>
2021-06-27 15:10:33 +0500

    Update main.py

--- scripts/main.py
+++ scripts/main.py
@@ -1,14 +1,14 @@
 from datetime import datetime
 from os import read
 
-user_data = ""
+user_data = "No Test"
 
 print("PRINT FROM PYTHON: ACTION 1.2 - STARTED")
 
 timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
-with open('user_data.txt', 'r') as f:
-    user_data = f.readline()
+#with open('user_data.txt', 'r') as f:
+#    user_data = f.readline()
 
 with open('output.txt', 'w') as f:
     f.write(f'{user_data}  # {timestamp}')

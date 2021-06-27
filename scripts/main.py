bf22a3559c0bb23f80b3bbfd33180fab16403a23
Andrei Shtanakov <45091754+andrei-shtanakov@users.noreply.github.com>
2021-06-27 13:47:04 +0500

    Add reading user data andoutput in file with  Timestamp

--- scripts/main.py
+++ scripts/main.py
@@ -1,10 +1,18 @@
-from datetime import datetime
+from os import read
+
+user_data = ""
 
 print("PRINT FROM PYTHON: ACTION 1.2 - STARTED")
 
 timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
+with open('user_data.txt', 'r') as f:
+    user_data = f .readline()
+
 with open('log.md', 'w') as f:
     f.write(f'# {timestamp}')
+
+with open('output.txt', 'w') as f:
+    f.write(f'{user_data}  # {timestamp}')
     
 print("PRINT FROM PYTHON: ACTION 1.2 - COMPLETED")
